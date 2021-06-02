7e80e4f87e7b5bda45a39ec0e5c6e3d3f2eb4579
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-02 13:46:02 -0300

    add new changes

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const form = document.querySelector(".wrapper form");
 const input = document.querySelector(".wrapper input");
 const msg = document.querySelector(".wrapper .msg");
 const list = document.querySelector(".cidade-section .cidades");
+// API Key Provedor do API https://openweathermap.org/
+const apiKey = "f6dcd8282da08edcc2032d01e33e35c1";
